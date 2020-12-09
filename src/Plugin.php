a859eb8804a9ce2a0dac95743151fbe52a1374f5
Eduard Kutuev <ed@napych.com>
2020-12-09 23:17:12 +0300

    Use View\HTML to inject scripts and styles

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,6 +2,9 @@
 
 namespace Difra\Fontawesome5;
 
+use Difra\Envi\Action;
+use Difra\Events\Event;
+
 /**
  * Class Plugin
  * @package Difra\Plugins\Fontawesome5
@@ -13,6 +16,14 @@ class Plugin extends \Difra\Plugin
      */
     protected function init()
     {
+        \Difra\Events\System::getInstance(Event::EVENT_RENDER_INIT)->registerHandler([static::class, 'addHTML']);
+    }
+
+    public static function addHTML(Event $event)
+    {
+        $html = Action::getController()->html;
+        $html->getHead()->addStylesheet('/fontawesome/css/fontawesome.css');
+        $html->getHead()->addStylesheet('/fontawesome/css/solid.css');
     }
 }
 
